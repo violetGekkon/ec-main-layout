7dfe2f2dd40e403703850328fe5a9bf89c6c7b89
sia <zaycevaao@yandex.ru>
2024-08-17 20:36:54 +0300

    change routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AppComponent} from "./app.component";
 
-export const routes: Routes = [];
+export const MAIN_ROUTES: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  }
+];
+export default MAIN_ROUTES;
